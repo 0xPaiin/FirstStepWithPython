5dc1c1d24ef09936ba8da7886b95edd96201262a
Pain <73969455+0xPaiin@users.noreply.github.com>
2024-12-29 11:58:37 +0530

    Update restrip_0.py

--- codewars/rstripFunc/restrip_0.py
+++ codewars/rstripFunc/restrip_0.py
@@ -1,10 +1,7 @@
-Numbers ending with zeros are boring.
-
-They might be fun in your world, but not here.
-
-Get rid of them. Only the ending ones.
-
-1450 -> 145
-960000 -> 96
-1050 -> 105
--1050 -> -105
+def no_boring_zeros(n):
+    if n == 0:
+        return 0
+    else:
+        num1 = str(n)
+        num1 = num1.rstrip("0")
+        return int(num1)
