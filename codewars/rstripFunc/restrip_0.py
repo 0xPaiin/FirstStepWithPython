4c2aede843b8ad337cbfe7e417014190011ad848
Pain <73969455+0xPaiin@users.noreply.github.com>
2024-12-29 12:13:09 +0530

    Update restrip_0.py

--- codewars/rstripFunc/restrip_0.py
+++ codewars/rstripFunc/restrip_0.py
@@ -5,3 +5,18 @@ def no_boring_zeros(n):
         num1 = str(n)
         num1 = num1.rstrip("0")
         return int(num1)
+
+
+#---------------another method---------------
+
+def no_boring_zeros(n):
+    try:
+        return int(str(n).rstrip("0"))
+    except ValueError:
+        print("Invalid input")
+
+try:
+    user_input = int(input("Enter your input: \n"))
+    print(no_boring_zeros(user_input))
+except Exception as e:
+    print("Invalid input")
